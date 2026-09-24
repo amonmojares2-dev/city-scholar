7c34b5e2035c29421e315056d3632ab40f4df286
amonmojares2 <amonmojares2-dev>
2026-09-25 07:30:25 +0800

    City Scholar login SMTP fix

--- server/utils/email.js
+++ server/utils/email.js
@@ -6,7 +6,10 @@ function getTransporter() {
     }
 
     return nodemailer.createTransport({
-        service: "gmail",
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
+        requireTLS: true,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASSWORD
